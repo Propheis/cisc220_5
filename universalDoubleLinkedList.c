9d39202cc05e9918fb279c12817846833f13f96e
Quentin Petraroia <q.petraroia@gmail.com>
2016-11-30 12:16:06 -0500

    Update universalDoubleLinkedList.c

--- universalDoubleLinkedList.c
+++ universalDoubleLinkedList.c
@@ -1,70 +1,104 @@
-#include<stdio.h>
+// Deven Bernard     - 10099810
+// Marissa Huang     - 10179169
+// Kevin Zuern       - 10134425
+// Quentin Petraroia - 10145835
 
-#define MODE_int    0;
-#define MODE_intP   1;
-#define MODE_float  2;
-#define MODE_floatP 3;
-#define MODE_char   4;
-#define MODE_charP  5;
+#include "udll.c"
+#include <stdlib.h>
 
-union Data {
-  int    intData;
-  int*   intPtrData;
+int main() {
+  //
+  // Setup
+  //
+  printf("Setting up testing...\n");
 
-  float  floatData;
-  float* floatPtrData;
-  
-  char   charData;
-  char*  charPtrData;
-};
+  int intData     = 1;
+  char charData   = 'a';
+  float floatData = 0.1;
 
-struct Node {
-  union Data data;
-  
-  // One of the MODE_* types. 
-  // Used to identify which type is being stored in data
-  char currentNodeType;
+  union Data a,b,c,d,e,f;
 
-  struct Node* next;
-  struct Node* before;
-};
+  a.intData      =  intData;
+  b.intPtrData   = &intData;
+  c.charData     =  charData;
+  d.charPtrData  = &charData;
+  e.floatData    =  floatData;
+  f.floatPtrData = &floatData;
 
-typedef struct Node Node;
-Node* head = NULL;
-Node* tail = NULL;
+  //
+  // Test insert()
+  //
 
-void Remove(int index) {
-  int indxPtr = 0;
-  for (Node *i = head; i!=NULL; i=i->next) {
-    if (indxPtr == (index -1)) {
-      indxPtr = indxPtr->next->next;
-    }
-    break;
+  printf("Testing insert() function\n");
+
+  // Inserting to last half of list, so should traverse from tail
+
+  insert(0, a, 0);
+  insert(1, b, 1);
+  insert(2, c, 2);
+  insert(3, d, 3);
+
+  // Inserting to first half of list, so should traverse from head
+  insert(0, e, 4);
+  insert(2, f, 5);
+  Node *i;
+  for(i = &head; i->next != NULL; i=i->next) {
+
+    if(i->type == 0) {
+    printf("%d", i->data.intData);
+  } else if (i->type == 1) {
+    printf("%x", i->data.intPtrData);
+  } else if (i->type == 2) {
+    printf("%c", i->data.charData);
+  } else if (i->type == 3) {
+    printf("%s", i->data.charPtrData);
+  } else if (i->type == 4) {
+    printf("%f", i->data.floatData);
+  } else {
+    printf("%x", i->data.floatPtrData);
   }
 }
-int main() {
-  Node n6 = {25, NULL, NULL}; head = &n6;
-  tail = &n6;
-  Node n5 = {1, &n6, NULL}; head = &n5;
-  n6.before = &n5;
-  Node n4 = {4, &n5, NULL}; head = &n4;
-  n5.before = &n4;
-	Node n3 = {2, &n4, NULL};  head = &n3;
-  n4.before = &n3;
-	Node n2 = {10, &n3, NULL};  head = &n2;
-  n3.before = &n2;
-	Node n1 = {3, &n2, NULL};  head = &n1;
-  n2.before = &n1;
 
-  Remove(4);
-	for(Node *i = head; i!=NULL; i=i->next){
-		printf("element = %d\n",i->data);
-	}
+  //
+  // Test length()
+  //
 
-  for(Node *i = tail; i!= NULL; i=i->before) {
-    printf("element = %d\n", i->data);
+  printf("Testing length() function\n");
+
+  if (length() != 6) {
+    printf("Something went wrong inserting nodes\n");
+    exit(1);
   }
+
+  //
+  // Test get()
+  //
+
+  printf("Testing get() function\n");
+
+  union Data result = get(0);
+  if (result.floatData != floatData)
+    printf("get() returned the wrong result or insert() is broken\n");
+
+  result = get(2);
+  if (result.floatPtrData != &floatData)
+    printf("get() returned the wrong result or insert() is broken\n");
+
+  //
+  // Test remove()
+  //
+
+  printf("Testing remove() function\n");
+
+  n_remove(0);
+
+  if (length() != 5)
+    printf("remove() didn't remove anything or length() is broken.\n");
+
+  if (get(0).intData != intData)
+    printf("remove() didn't remove the right element or get() is broken\n");
+
+  printf("Testing complete. Exiting now\n");
+
   return 0;
-
-
 }
